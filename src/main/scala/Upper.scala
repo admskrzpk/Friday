0af69b982347b581bcda4acc7970ad8e0c209c3d
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-08 15:15:36 +0200

    update

--- src/main/scala/Upper.scala
+++ src/main/scala/Upper.scala
@@ -1,23 +1,24 @@
 
 object Upper extends App {
+
   import org.apache.spark.sql.SparkSession
-  import org.apache.spark.sql.functions.{col, upper}
+  import org.apache.spark.sql.functions._
+
 
   val spark = SparkSession
     .builder()
     .master("local[*]")
     .getOrCreate()
-  val path = args(0)
+  val path = if (args.length > 0) args(0)
+  else "C:\\spark\\Friday\\Sample100.csv"
+  val columnName = args(1)
 
   val sample100 = spark
     .read
     .option("header", value = true)
     .option("inferSchema", value = true)
     .csv(path)
-    .toDF()
 
-  val columnName = args(1)
+  val result = sample100.withColumn("upper_" + columnName, upper(col(columnName))).show()
 
-
-//    val result = sample100.withColumn("upper", upper(col("city"))).show()
-}
+  }
